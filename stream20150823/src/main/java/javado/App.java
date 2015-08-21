8ffc56d650142e2be0d7b2abf877ded46a06b2a3
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2015-08-22 00:31:10 +0900

    groupingBy, flatmapの演習を修正、テキストの都道府県を処理しやすいように修正

--- stream20150823/src/main/java/javado/App.java
+++ stream20150823/src/main/java/javado/App.java
@@ -1,13 +1,14 @@
 package javado;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class App {
 
-  private Service service;
+  private List<Profile> profiles;
 
   public static void main(String[] args) {
     App app = new App();
@@ -22,10 +23,12 @@ public class App {
     app.example8();
     app.example9();
     app.example10();
+    app.example11();
+    app.example12();
   }
 
   public App() {
-    service = new Service();
+    profiles = new Service().fetchProfiles();
   }
 
   public void view(Object object) {
@@ -40,14 +43,12 @@ public class App {
 
   public void example0() {
     System.out.println("------------ example0 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     view(profiles);
   }
 
   // 演習1: 女性(sex==1)の人を抜き出す
   public void example1() {
     System.out.println("------------ example1 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     List<Profile> choiced = profiles.stream()
         .filter(p -> p.getSex() == 1)
         .collect(Collectors.toList());
@@ -57,7 +58,6 @@ public class App {
   // 演習2: 女性(sex==1)で、二十歳以上の人を抜き出す
   public void example2() {
     System.out.println("------------ example2 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     List<Profile> choiced = profiles.stream()
         .filter(p -> p.getSex() == 1)
         .filter(p -> p.getAge() >= 20)
@@ -68,7 +68,6 @@ public class App {
   // 演習3: 女性(sex==1)で、二十歳以上の人の名前を抜き出す
   public void example3() {
     System.out.println("------------ example3 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     List<String> mapped = profiles.stream()
         .filter(p -> p.getSex() == 1)
         .filter(p -> p.getAge() >= 20)
@@ -80,7 +79,6 @@ public class App {
   // 演習4: 女性(sex==1)で、二十歳以上を抜き出し、年齢順にソートする
   public void example4() {
     System.out.println("------------ example4 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     List<Profile> choiced = profiles.stream()
         .filter(Profile::isFamale)
         .filter(Profile::isAdult)
@@ -92,19 +90,17 @@ public class App {
   // 演習5: 男性(sex==0)で、未成年人を抜き出し、年齢順にソートする
   public void example5() {
     System.out.println("------------ example5 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     List<Profile> choiced = profiles.stream()
         .filter(Profile::isMale)
         .filter(Profile::isUnderage)
-        .sorted(Comparator.comparing(Profile::getAge).reversed())
+        .sorted(Comparator.comparing(Profile::getAge))
         .collect(Collectors.toList());
     view(choiced);
   }
 
-  // 演習6: 男性(sex==0)で、未成年人を抜き出し、年齢順にソートする
+  // 演習6: 男性(sex==0)で、未成年人を抜き出し、年齢順に逆ソートする
   public void example6() {
     System.out.println("------------ example6 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     List<Profile> choiced = profiles.stream()
         .filter(Profile::isMale)
         .filter(Profile::isUnderage)
@@ -116,9 +112,7 @@ public class App {
   // 演習7: カレーの食べ方"せき止め派"のうち、最初の一人を取り出す
   public void example7() {
     System.out.println("------------ example7 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     Profile choiced = profiles.stream()
-        .parallel()
         .filter(p -> p.getPolicy().matches(".*せき止め派.*"))
         .findFirst().get();
     view(choiced);
@@ -127,7 +121,6 @@ public class App {
   // 演習8: カレーの食べ方"せき止め派"の人数を出す
   public void example8() {
     System.out.println("------------ example8 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     Profile choiced = profiles.stream()
         .parallel()
         .filter(p -> p.getPolicy().matches(".*せき止め派.*"))
@@ -138,20 +131,39 @@ public class App {
   // 演習9: カレーの食べ方"せき止め派"の人数を出す
   public void example9() {
     System.out.println("------------ example9 ----------");
-    List<Profile> profiles = service.fetchProfiles();
     Long choiced = profiles.stream()
         .filter(p -> p.getPolicy().matches(".*せき止め派.*"))
         .count();
     view(choiced);
   }
 
-  // 演習10: flatMapで九九の計算
+  // 演習10: 都道府県でグルーピングする
   public void example10() {
     System.out.println("------------ example10 ----------");
-    IntStream.rangeClosed(1, 9)
-        .flatMap(i -> IntStream.rangeClosed(1, 9)
-            .map(j -> i * j))
-        .forEach(System.out::println);
-    ;
+    Map<String, List<Profile>> choiced = profiles.stream()
+        .collect(Collectors.groupingBy(Profile::getState));
+    view(choiced);
+  }
+
+  // 演習11: 都道府県で名前だけグルーピングする
+  // 長いので、Collectors.*をstatic importすると楽
+  public void example11() {
+    System.out.println("------------ example11 ----------");
+    Map<String, List<String>> choiced = profiles.stream()
+        .collect(Collectors.groupingBy(Profile::getState, Collectors.mapping(Profile::getName, Collectors.toList())));
+    view(choiced);
+  }
+
+  // 演習12: リストに存在するカレーの食べ方を全てリストアップする
+  public void example12() {
+    System.out.println("------------ example12 ----------");
+    List<String> choiced = profiles.stream()
+        .map(Profile::getPolicy)
+        .map(s -> s.split("・"))
+        .flatMap(strs -> Arrays.stream(strs))
+        .distinct()
+        .collect(Collectors.toList());
+    view(choiced);
+
   }
 }
